4cb49e942e3726ffcd2ed306d01ce589dcba223d
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-02 10:59:08 +0400

    added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const arrow__up = document.getElementById('swiper-button-next');
+const number_slide = document.getElementById('main__number__p');
+const arrow__down = document.getElementById('swiper-button-prev');
+
+const circle = document.getElementById('circle__wrapper');
+
