70eda6bba637ef12f2c95ef4c081b3df4a9a65a5
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-02 11:24:30 +0400

    фввув

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const arrow__down = document.getElementById('swiper-button-prev');
 
 const circle = document.getElementById('circle__wrapper');
 
+const number = 1;
+number_slide.textContent = number;
+
+function slider__number(){
+    if(number <= 3){
+        numner++;
+    }
+}
