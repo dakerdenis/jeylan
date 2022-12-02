107298c6c8e3586620ab089cc32edcc9815a33e2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-02 12:09:42 +0400

    added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
-const arrow__up = document.getElementById('swiper-button-next');
-const number_slide = document.getElementById('main__number__p');
-const arrow__down = document.getElementById('swiper-button-prev');
+const arrow__up = document.querySelector('#swiper-button-next');
+const number_slide = document.querySelector('#main__number__p');
+const arrow__down = document.querySelector('#swiper-button-prev');
 
-const circle = document.getElementById('circle__wrapper');
+const circle = document.querySelector('#circle__wrapper');
 
-const number = 1;
+let number = 1;
+let max_number = 4;
+let rotation = 0;
+arrow__up.addEventListener('click', function (){
+    slider__number_up();
+});
+arrow__down.addEventListener('click', function (){
+    slider__number_down();
+});
 number_slide.textContent = number;
+function slider__number_up(){
+    if(number <= max_number){
+        number = number + 1;
+        number_slide.textContent = number;
 
-function slider__number(){
-    if(number <= 3){
-        numner++;
+        rotation = rotation + 45;
+        var str =  rotation.toString();
+
+        circle.style.rotate = str +"deg";
     }
+    
+}
+function slider__number_down(){
+
+    if(number >= 1){
+        number = number - 1;
+        number_slide.textContent = number;
+        rotation = rotation - 45 ;
+        var str =  rotation.toString();
+
+        circle.style.rotate = str +"deg";
+    }
+    
+    
 }
